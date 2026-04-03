e59bf2d4be87496c3fcb9eb7747ff64b8517cbb4
henrythierrydev <henrythierrydev@gmail.com>
2026-04-03 19:35:18 -0300

    :recycle: refactor(core): rename shard.utils to shard.util (singular suffix)

--- packages/core/src/client/index.ts
+++ packages/core/src/client/index.ts
@@ -2,7 +2,7 @@ export { ClientFactory } from './client.factory';
 export { ParentHookRegistry } from './parent-hook.registry';
 export { PresenceManager } from './presence.manager';
 export { SpraxiumShardManager } from './shard.manager';
-export { isShardChild } from './shard.utils';
+export { isShardChild } from './shard.util';
 export * from './constants';
 export * from './interfaces';
 export * from './types';
